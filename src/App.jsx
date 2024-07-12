c092ca8324c66d5acdfda6ae5785591c743ca2f9
safi <safiou102003@gmail.com>
2024-07-12 13:16:28 -0700

    fix:resonsive

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ function App() {
   return (
     <div className="bg-bgPrimary">
 
-    <div className="m-5 lg:pb-[200px] lg:pl-[200px] lg:pr-[200px]  ">
+    <div className="m-5 lg:pb-[70px] lg:pl-[70px] lg:pr-[70px] 
+     xl:pb-[150] xl:pl-[150] xl:pr-[150] 2xl:pb-[200px] 2xl:pl-[200px] 2xl:pr-[200px] ;
+    ">
 
       <Nav />
       <Hero/>
